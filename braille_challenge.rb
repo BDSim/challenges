71767022e1d2c22df382b94ebd0e5fc1cce42aad
BDSim <kc9fle@gmail.com>
2013-12-26 12:44:38 -0600

    Reddit Braille Challenge

--- braille_challenge.rb
+++ braille_challenge.rb
@@ -1,22 +1,20 @@
 #Braille Challenge
 #2x6 space-delimited characters
 require 'pry-byebug'
-val1, val2, val3 = []
+
 val1 = gets.chomp.split(" ")
 val2 = gets.chomp.split(" ")
 val3 = gets.chomp.split(" ")
 
 def braille (val1, val2, val3)
-    code = []
     i = 0
     while val1[i] != nil
-    code << val1[i] << val2[i] << val3[i]
+    code = [] << val1[i] << val2[i] << val3[i]
     i += 1
     end
     decode(code)
 end
 def decode (code)
-    letters = []
     while code.empty? == false
         word = code.shift(3).join("")
         alphabet = {'0.....' => 'a', '0.0...' => 'b', '00....' => 'c', 
@@ -28,7 +26,7 @@ def decode (code)
                     '.00.0.' => 's', '.0000.' => 't', '0...00' => 'u',
                     '0.0.00' => 'v', '.000.0' => 'w', '00..00' => 'x',
                     '00.000' => 'y', '0..000' => 'z'}
-        letters << alphabet[word]
+        letters = [] << alphabet[word]
     end
     english = letters.join("")
     output(english)
