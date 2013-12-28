25440d96f1e31455f44f093696dc8fe332e47528
BDSim <kc9fle@gmail.com>
2013-12-28 16:42:45 -0600

    Reddit Nuts and Bolts Challenge

--- nutsbolts_challenge4.rb
+++ nutsbolts_challenge4.rb
@@ -1,13 +1,10 @@
 #Nuts and Bolts
-require 'pry-byebug'
-
 rows = gets.chomp.to_i
 items, p = {}, []
 rows.times do
     item, price = gets.chomp.split
     items[item] = price.to_i
 end
-p = []
 rows.times do
     item, price = gets.chomp.split
     if items[item] != price.to_i
